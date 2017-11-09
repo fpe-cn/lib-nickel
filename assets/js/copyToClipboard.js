aeb20bccbb9893fe389147e2d72a59013363814e
Mélissa <melissa.erades@compte-nickel.fr>
2017-11-09 15:34:43 +0100

    bugfix domlib vs jquery

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -1,9 +1,8 @@
-var $ = require('../../assets/js/dom');
+let $$ = require('../../assets/js/dom');
 
 module.exports.init = function () {
-    $('[data-role="copy-to-clipboard"]').on('click', (e) => {
-        let copy = $(e.target).parent('.input-group').select('input')[0].select();
-        console.log(copy);
+    $$('[data-role="copy-to-clipboard"]').on('click', (e) => {
+        let copy = $$(e.target).parent('.input-group').select('input')[0].select();
         document.execCommand('copy');
         return false;
     });
