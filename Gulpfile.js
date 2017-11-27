3ff847509dd3a9689bf0ce167d71e3303285b10e
Mélissa <melissa.erades@compte-nickel.fr>
2017-11-27 17:57:43 +0100

    add modules to create automate release

--- Gulpfile.js
+++ Gulpfile.js
@@ -24,6 +24,7 @@ var babelify = require('babelify');
 var run = require('gulp-run');
 var bump = require('gulp-bump');
 var git = require('gulp-git');
+var fs = require('fs');
 
 gulp.task('css', function () {
     return gulp.src('sandbox/scss/*.scss')
@@ -167,28 +168,8 @@ gulp.task('webserver', function () {
 
 gulp.task('run-test', function() {
     return run('npm test').exec();
-})
-
-// version: 1.0.0
-gulp.task('bump-major', function () {
-    return gulp.src('./package.json')
-        .pipe(bump({type: 'major'}))
-        .pipe(gulp.dest('./'));
 });
 
-// version: 0.1.0
-gulp.task('bump-minor', function () {
-    return gulp.src('./package.json')
-        .pipe(bump({type: 'minor'}))
-        .pipe(gulp.dest('./'));
-});
-
-// version: 0.0.1
-gulp.task('bump', function () {
-    return gulp.src('./package.json')
-        .pipe(bump({type: 'patch'}))
-        .pipe(gulp.dest('./'));
-});
 /*
  * Tests : npm test
  */
@@ -209,3 +190,74 @@ gulp.task('work', function(cb) {
     runSequence('run-test', ['font', 'css', 'js', 'html', 'img-sandbox'], ['webserver', 'watch'], cb)
 });
 gulp.task('default', ['work']);
+
+
+
+
+
+
+
+
+
+
+
+
+/** AUTOMATE RELEASE **/
+gulp.task('github-release', function(done) {
+    conventionalGithubReleaser({
+        type: "oauth",
+        token: '22349c1d12523e9eba7fe288249cb2e84ba414f9' // change this to your own GitHub token or use an environment variable
+    }, {
+        preset: 'lib-nickel' // Or to any other commit message convention you use.
+    }, done);
+});
+
+gulp.task('bump-version', function () {
+    return gulp.src(['./package.json'])
+        .pipe(bump({type: "patch"}).on('error', gutil.log))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+    return gulp.src('.')
+        .pipe(git.add())
+        .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+    git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+    var version = getPackageJsonVersion();
+    git.tag(version, 'Created Tag for version: ' + version, function (error) {
+        if (error) {
+            return cb(error);
+        }
+        git.push('origin', 'master', {args: '--tags'}, cb);
+    });
+
+    function getPackageJsonVersion () {
+        // We parse the json file instead of using require because require caches
+        // multiple calls so the version number won't be updated
+        return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+    };
+});
+
+gulp.task('release', function (callback) {
+    runSequence(
+        'bump-version',
+        'changelog',
+        'commit-changes',
+        'push-changes',
+        'create-new-tag',
+        'github-release',
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            } else {
+                console.log('RELEASE FINISHED SUCCESSFULLY');
+            }
+            callback(error);
+        });
+});
