691e28b21dd5888871dd675456bccb7a1f01326e
Mélissa <melissa.erades@compte-nickel.fr>
2017-11-28 10:11:03 +0100

    [Prerelease] Bumped version number

--- Gulpfile.js
+++ Gulpfile.js
@@ -165,7 +165,7 @@ gulp.task('webserver', function () {
 
 gulp.task('run-test', function() {
     return run('npm test').exec();
-})
+});
 
 /*
  * Tests : npm test
@@ -187,3 +187,80 @@ gulp.task('work', function(cb) {
     runSequence('run-test', ['font', 'css', 'js', 'html', 'img-sandbox'], ['webserver', 'watch'], cb)
 });
 gulp.task('default', ['work']);
+
+
+
+
+
+
+
+
+
+
+
+
+/** AUTOMATE RELEASE **/
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var fs = require('fs');
+var conventionalGithubReleaser = require('conventional-github-releaser');
+var envs = require('envs');
+
+gulp.task('github-release', function(done) {
+    conventionalGithubReleaser({
+        type: "oauth",
+        token: envs('GITHUB_TOKEN_LIB_NICKEL')
+    }, {
+        preset: 'lib-nickel'
+    }, done);
+});
+
+gulp.task('bump-version', function () {
+    return gulp.src(['./package.json'])
+        .pipe(bump({type: "patch"}).on('error', gutil.log))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+    return gulp.src('.')
+        .pipe(git.add())
+        .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+    git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+    var version = getPackageJsonVersion();
+    git.tag(version, 'Created Tag for version: ' + version, function (error) {
+        if (error) {
+            return cb(error);
+        }
+        git.push('origin', 'master', {args: '--tags'}, cb);
+    });
+
+    function getPackageJsonVersion () {
+        // We parse the json file instead of using require because require caches
+        // multiple calls so the version number won't be updated
+        return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+    };
+});
+
+gulp.task('release', function (callback) {
+    console.log(envs('GITHUB_TOKEN_LIB_NICKEL'), process.env['GITHUB_TOKEN_LIB_NICKEL']);
+    runSequence(
+        'bump-version',
+        'commit-changes',
+        'push-changes',
+        'create-new-tag',
+        'github-release',
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            } else {
+                console.log('RELEASE FINISHED SUCCESSFULLY');
+            }
+            callback(error);
+        });
+});
