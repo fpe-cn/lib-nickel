b56a97bf8f32d5d8f430b9dd15f13f604122c518
Mélissa <melissa.erades@compte-nickel.fr>
2017-11-29 11:41:42 +0100

    refact copyToClipboard

--- assets/js/copyToClipboard.js
+++
@@ -1,9 +0,0 @@
-let $ = require('./dom');
-
-module.exports.init = function () {
-    $('[data-role="copy-to-clipboard"]').on('click', (e) => {
-        let copy = $(e.target).parent('.input-group').select('input')[0].select();
-        document.execCommand('copy');
-        return false;
-    });
-};
