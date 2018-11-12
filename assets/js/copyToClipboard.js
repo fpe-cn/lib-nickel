8971c7cf7579f2230de751e6a5ff776e6a674a91
Hadrien QUENIN <hquenin@MacBook-Pro-de-Service-2.local>
2018-11-12 11:04:28 +0100

    update lib referentiel

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -2,7 +2,7 @@ let $ = require('./dom');
 
 module.exports.init = function () {
     $('[data-role="copy-to-clipboard"]').on('click', (e) => {
-        let copy = $(e.target).parent('.input-group').select('input')[0].select();
+        let copy = $(e.target).parent('.cn-input-group').select('input')[0].select();
         document.execCommand('copy');
         return false;
     });
--- sandbox/js/default.js
+++ sandbox/js/default.js
@@ -1,6 +1,4 @@
-// const MDCTabBar = require('@material/tab-bar')
-require('prismjs')
-require('../../assets/js/copyToClipboard').init()
-require('../../assets/js/toggler-menu').init()
-
-// const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+import 'prismjs';
+require('../../assets/js/copyToClipboard').init();
+require('../../assets/js/toggler-menu').init();
+import './mdc-drawer';
